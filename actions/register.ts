b752865491ca00319f9d97c8aaf467359affe658
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-10-22 12:34:30 +0700

    refactor(auth): move email check function to data/user.ts

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import { db } from "@/lib/db"
 import { z } from "zod"
 import { RegisterSchema } from "@/schemas"
 import bcrypt from "bcrypt"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values)
@@ -13,11 +14,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const { name, email, password } = validateFields.data
   const hashedPassword = await bcrypt.hash(password, 10)
 
-  const existingUser = await db.user.findUnique({
-    where: {
-      email
-    }
-  })
+  const existingUser = await getUserByEmail(email)
 
   if (existingUser) return { error: "Email already in use!" }
 
