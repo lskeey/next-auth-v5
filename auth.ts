05fd39f24c2fbb7cd39a309ed080aa976ea03508
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-10-25 13:35:01 +0700

    feat(auth): add session and JWT callbacks in auth.ts

--- auth.ts
+++ auth.ts
@@ -3,9 +3,32 @@ import authConfig from "./auth.config"
 
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/db"
+import { getUserById } from "./data/user"
+import { UserRole } from "@prisma/client"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   ...authConfig,
-  session: { strategy: "jwt" }
+  session: { strategy: "jwt" },
+  callbacks: {
+    async session({ token, session}) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+      return token
+    }
+  }
 })
